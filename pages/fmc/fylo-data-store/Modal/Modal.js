318c7d139f7077f7ad134a15718714844ce4485d
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-07-22 11:58:24 -0300

    alterado o modal do Fylo Data Store component

---
+++ pages/fmc/fylo-data-store/Modal/Modal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+
+import { IoMdConstruct } from "react-icons/io";
+
+import styles from "./Modal.module.scss";
+import animation from "../../../api/animation.module.scss";
+
+const Modal = (props) => {
+  return (
+    <CSSTransition
+      in={props.open}
+      timeout={200}
+      classNames={animation}
+      unmountOnExit
+    >
+      <div className={styles.modal}>
+        <div className={styles.top}>
+          <IoMdConstruct className={styles.icon} />
+          <p className={styles.text}>These icons are just decoration</p>
+        </div>
+        <button className={styles.button} onClick={props.close}>
+          DISMISS
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+export default Modal;
